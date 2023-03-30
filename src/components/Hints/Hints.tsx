1dc8291ca89a4ae2fa219bf14599de4b4b731956
Arejas <62893479+arejasverduras@users.noreply.github.com>
2023-03-30 13:21:40 +0200

    copy input and hints components

---
+++ src/components/Hints/Hints.tsx
@@ -0,0 +1,80 @@
+import styles from './Hints.module.css';
+import { AnimatePresence, motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {  faQuestion } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+
+interface HintsProps {
+    visible: boolean,
+    setSearchTerm: (term:string) => void;
+}
+
+export const Hints = ({visible, setSearchTerm}:HintsProps) => {
+    const [hints, setHints] = useState(false);
+    const toggleHints = () => {
+        setHints(!hints);
+      }
+    
+    const animations = {
+        hintsEnter: {
+            opacity: [0,1],
+            y: [-20,0]
+        },
+        hintsExit: {
+            opacity: [1,0],
+            y: [0,-20]
+        },
+        hintsRotate: {
+            rotate: [0,90]
+        }
+    }
+
+    const hintsList = ['home','about','projects'];
+
+    const generateLinks = (list: string[]) => {
+        return list.map((item, index) => 
+            <li key="index" onClick={()=>{setSearchTerm(item)}}>{item}</li>
+        )
+    }
+
+    const handleClick = ({target}:any) => {
+        setSearchTerm(target.value);
+    }
+    
+    return (
+        <AnimatePresence>        
+            {visible && (
+                        <motion.div 
+                            className={styles.hintsHolder} 
+                            onClick={toggleHints}
+                            key="hintsHolder"
+                            animate={{opacity: [0,1], y: [20,0]}}
+                            transition={{delay: 1.2}}
+                            >
+                            <motion.div
+                                className={styles.hintsArrow}
+                                key="hintsArrow"
+                                variants={animations}
+                                initial={{rotate:0}}
+                                animate={hints? "hintsRotate": "hintsRotateBack"}
+                                >
+                                    <FontAwesomeIcon icon={faQuestion}/>
+                                </motion.div>
+                            </motion.div>)}
+                    {hints && (
+                    <>
+                    {/* <motion.div>Commands</motion.div> */}
+                    <motion.ul 
+                        key="hintsList"
+                        className={styles.hintsList}
+                        layout
+                        variants={animations}
+                        animate="hintsEnter"
+                        exit="hintsExit"
+                        >
+                            {generateLinks(hintsList)}
+                    </motion.ul>
+                    </>)}
+                </AnimatePresence>
+    )
+}
---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,144 @@
+import styles from './Input.module.css';
+import { AnimatePresence, motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTurnDown } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+interface InputProps {
+    visible: boolean, 
+    toggleVisible: ()=>void,
+    searchTerm: string,
+    setSearchTerm: (term:string) => void,
+    startOpen?: boolean,
+}
+
+export const Input = ({visible, toggleVisible, searchTerm, setSearchTerm, startOpen}:InputProps) => {
+    const [message, setMessage] = useState('');
+    const [showMessage, setShowMessage] = useState(false);
+    
+    const samePageMessage = `You are currently on this page`;
+    const errormessage = `command: ${searchTerm} not found. Click on the '?' icon to see a list of commands`
+    const router = useRouter();
+
+    const animations = {
+        enter: {
+            scale: [0.1,1]
+        },
+        exit: {
+            scale: 0
+        },
+        slideOpen: {
+            width: 200,
+        },
+        slideClose: {
+            width: 50
+        }
+    }
+
+    useEffect(()=>{
+        const inpute = document.getElementById("inputfield");
+        console.log(inpute);
+        inpute?.focus();
+    
+      },[visible]
+      );
+
+
+      const handleChange = ({target}:any) => {
+        setShowMessage(false);
+        setSearchTerm(target.value)
+      }
+      
+      const handleKeyDown = (event:any) => {
+        if (event.key === 'Enter') {
+            navigate();
+        }
+      }
+
+      const navigate = () => {
+        console.log(router.asPath);
+        if (`/${searchTerm}` === router.asPath){
+            setMessage(samePageMessage);
+            setShowMessage(true);
+            return;
+        }
+        
+        switch (searchTerm) {
+            case "about":    
+                router.push('/about')
+                break;
+            case "home":
+                router.push('/test')
+                break;
+            case "projects": 
+                router.push('/projects');
+                break;
+            default:
+            setMessage(errormessage);    
+            setShowMessage(true);
+                break;
+      }
+      }
+
+
+    return (
+        <div className={styles.inputHolder}>
+                <AnimatePresence
+                    mode="wait"
+                    >
+                    <motion.div
+                        className={styles.dot}
+                        key="dot"
+                        variants={animations}
+                        animate="enter"
+                        exit="exit"
+                        onClick={toggleVisible}
+        
+                        layoutId="inputDot"
+                        >
+                        </motion.div>
+                        {visible && (
+                            <motion.div
+                                className={styles.input}
+                                key="input"
+                                variants={animations}
+                                initial={startOpen? {width: 200}: {width: 50}}
+                                animate={!startOpen && "slideOpen"}
+                                // exit="slideClose"
+                                layoutId="inputBar"
+                                >
+                                    <div
+                                        >{'>'} </div>
+                                    <input 
+                                        id="inputfield"
+                                        key="inputfield"
+                                        placeholder="about"
+                                        className={styles.inputField}
+                                        value={searchTerm}
+                                        onChange={handleChange}
+                                        onKeyDown={handleKeyDown}
+                                        />
+                                        <motion.button 
+                                            type="submit"
+                                            key="inputButton"
+                                            onClick={navigate}
+                                            animate={{x: [40,0], opacity: [0,1]}}
+                                            transition={{delay: 0.4}}
+                                            >Enter <FontAwesomeIcon icon={faTurnDown}/></motion.button>
+                                </motion.div>
+                        )}
+                </AnimatePresence>
+                <AnimatePresence>
+                    {showMessage && (
+                        <motion.p
+                            key="errormessage"
+                            animate={{x: [20,0], opacity: [0,1]}}
+                            exit={{x: [0,-20], opacity: [1,0]}}
+                            >{message}</motion.p>
+                    )}
+                </AnimatePresence>
+                
+            </div>
+    )
+}
