e6406a344d8c769c653efb7a8e9baceb49901b03
Arejas <62893479+arejasverduras@users.noreply.github.com>
2023-04-12 13:10:27 +0200

    cleanup code

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -14,51 +14,48 @@ export default function PageLayout({children}:any) {
     const [showMessage, setShowMessage] = useState(false);
     
     return (
-        <div 
-            className={styles.container}
-            >
+        <div className={styles.container}>
             <motion.div 
                 className={styles.header}
                 key="PageLayoutHeader"
                 animate={{y: [-200,0]}}
                 transition={{delay: 0.2, ease: 'anticipate' }}
                 >
-                    <div className={styles.logo}>
-                        Michiel Roukens
-                    </div>
-                </motion.div>
-                <motion.div className={styles.commandHolder}>
-                <Input 
-                        startOpen
-                        visible={visible}
-                        toggleVisible={()=>{setVisible(!visible)}}
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                        setMessage={setMessage}
-                        setShowMessage={setShowMessage}
-                        />
-                    </motion.div>
-                    <Message 
-                        message={message}
-                        showMessage={showMessage}
-                        />
-                    <motion.div 
-                        className={styles.hintsHolder}
-                        key="hintsHolder"
-                        layout
-                        >
-                            <Hints 
-                            visible={true}
-                            setSearchTerm={setSearchTerm}
-                            setShowMessage={setShowMessage}
-                            />
-                        </motion.div>
-                <main
-                    >
-                    <AnimatePresence mode="wait">
-                        {children}
-                    </AnimatePresence>
-                </main>
+                <div className={styles.logo}>
+                    Michiel Roukens
+                </div>
+            </motion.div>
+            <motion.div className={styles.commandHolder}>
+            <Input 
+                startOpen
+                visible={visible}
+                toggleVisible={()=>{setVisible(!visible)}}
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                setMessage={setMessage}
+                setShowMessage={setShowMessage}
+                />
+            </motion.div>
+            <Message 
+                message={message}
+                showMessage={showMessage}
+                />
+            <motion.div 
+                className={styles.hintsHolder}
+                key="hintsHolder"
+                layout
+                >
+                <Hints 
+                    visible={true}
+                    setSearchTerm={setSearchTerm}
+                    setShowMessage={setShowMessage}
+                    />
+            </motion.div>
+            <main>
+                <AnimatePresence mode="wait">
+                    {children}
+                </AnimatePresence>
+            </main>
         </div>
     )
 }
