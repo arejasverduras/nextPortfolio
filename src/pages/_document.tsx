3f80f8d5356a97821f2f842fe1ff3565ef37ac57
Arejas <62893479+arejasverduras@users.noreply.github.com>
2023-04-18 17:36:04 +0200

    add theme-color based on variable

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <link rel="manifest" href="/manifest.json"/> */}
+        <meta name="theme-color" content="var(--colorHeaderBg)" />
+        <meta name="apple-mobile-web-app-status-bar" content="var(--colorHeaderBg)" />
+      </Head>
       <body>
         <Main />
         <NextScript />
